7805315f50b8785dc965eb44298406a3851fa7bb
joseph <yusufaktok@gmail.com>
2022-07-14 14:42:58 +0300

    structure tidy up

--- blog/src/main/resources/blog.sql
+++
@@ -1,7 +0,0 @@
-select a.author_id,author_name,p.post_id,post_content,c.commentator_id,comment_content,comment_time from 
-"Blog"."Comment" as c,
-"Blog"."Author" as a,
-"Blog"."Post" as p ,
-"Blog"."Commentator" as cr
-where
-a.author_id = p.author_id and c.post_id = p.post_id and cr.commentator_id = c.commentator_id
--- blog/src/main/resources/db/migration/V1_1__add_account.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists account(
-
-    user_id varchar primary key,
-    username varchar(255) unique not null,
-    country varchar(128) not null
-)
